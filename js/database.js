579bd14a32d0ed9199b11abef7aed7efe54bf28e
Victor Cruz <motoggb979@gmail.com>
2022-05-19 14:13:33 -0300

    projeto finalizado

--- js/database.js
+++ js/database.js
@@ -3,7 +3,7 @@
 const data = [
   {
     id: 1,
-    img: "../img/headset-gamer-superframe.jpg",
+    img: "./img/headset-gamer-superframe.jpg",
     nameItem: "HEADSET GAMER",
     description:
       "Vire um gamer de verdade com o headset Pragma, microfone embutido e luzes RGB...",
@@ -13,7 +13,7 @@ const data = [
   },
   {
     id: 2,
-    img: "../img/mouse-redragon-gamer-cobra-chroma-m711-7.webp",
+    img: "./img/mouse-redragon-gamer-cobra-chroma-m711-7.webp",
     nameItem: "Redragon Cobra M711",
     description:
       "O mouse gamer Cobra Chroma da Redragon, traz diversos fatores que ajudam os gamers...",
@@ -23,7 +23,7 @@ const data = [
   },
   {
     id: 3,
-    img: "../img/cadeira-gamer-ninja-kage-branco-e-rosa.jpg",
+    img: "./img/cadeira-gamer-ninja-kage-branco-e-rosa.jpg",
     nameItem: "Ninja Kage, Branco e Rosa",
     description:
       "desenvolvida especialmente para gamers. Com espuma de alta densidade revestida por...",
@@ -33,7 +33,7 @@ const data = [
   },
   {
     id: 4,
-    img: "../img/mouse-gamer-redragon-cobra-m711-rgb-10000-dpi-7-botoes-programaveis-white_114701.webp",
+    img: "./img/mouse-gamer-redragon-cobra-m711-rgb-10000-dpi-7-botoes-programaveis-white_114701.webp",
     nameItem: "Cobra M711W RGB",
     description:
       "O Mouse Redragon M711 tem níveis de DPI ajustáveis que atendem às suas múltiplas necessidades...",
@@ -43,7 +43,7 @@ const data = [
   },
   {
     id: 5,
-    img: "../img/headset-gamer-marvo-scorpion.webp",
+    img: "./img/headset-gamer-marvo-scorpion.webp",
     nameItem: "Marvo HG8928, 3.5mm",
     description:
       "Verificando pontos-chave para jogar o máximo de detalhes permite a rápida identificação do inimigo...",
@@ -53,7 +53,7 @@ const data = [
   },
   {
     id: 6,
-    img: "../img/mouse-gamer-hyperx-pulsefire-surge.jpg",
+    img: "./img/mouse-gamer-hyperx-pulsefire-surge.jpg",
     nameItem: "HyperX Pulsefire Surge",
     description:
       "O HyperX Pulsefire Surge oferece aos jogadores o melhor em estilo e conteúdo...",
@@ -63,7 +63,7 @@ const data = [
   },
   {
     id: 7,
-    img: "../img/cadeira-gamer-superframe-cleric-reclinavel-4d.jpg",
+    img: "./img/cadeira-gamer-superframe-cleric-reclinavel-4d.jpg",
     nameItem: "SuperFrame Cleric, Reclinável",
     description:
       "desenvolvida para horas de jogatina. Com acolchoado revestido por um tecido de altíssima qualidade...",
@@ -73,7 +73,7 @@ const data = [
   },
   {
     id: 8,
-    img: "../img/cadeira-gamer-elements-gaming-elemental-terra-verde.webp",
+    img: "./img/cadeira-gamer-elements-gaming-elemental-terra-verde.webp",
     nameItem: "Elements Elemental Terra, Green",
     description:
       "inbutindo conceitos de MMORPG, RPG e Fantasia Medieval...",
